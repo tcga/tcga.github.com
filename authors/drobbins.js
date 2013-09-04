4bcd65eab4fbc4f9f597dde30d163bb3a4fea2a3
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2013-09-04 16:56:26 -0500

    Add David's manifest.

---
+++ authors/drobbins.js
@@ -0,0 +1,17 @@
+manifest({
+  "https://raw.github.com/drobbins/TCGA.query/master/tcgaquery.js" : {
+    "name" : "TCGA Query",
+    "description" : "SPARQL Interface for the TCGA Roadmap",
+    "url" : "https://raw.github.com/drobbins/TCGA.query/master/tcgaquery.js"
+  },
+  "https://raw.github.com/drobbins/TCGA.ql/master/tcgaql.js" : {
+    "name" : "TCGA Query Language",
+    "description" : "A simple query builder for the TCGA Roadmap",
+    "url" : "https://raw.github.com/drobbins/TCGA.ql/master/tcgaql.js"
+  },
+  "https://raw.github.com/drobbins/TCGA.qlui/master/tcgaqlui.js" : {
+    "name" : "TCGA Query Language UI",
+    "description" : "GUI for TCGA.ql",
+    "url" : "https://raw.github.com/drobbins/TCGA.qlui/master/tcgaqlui.js"
+  }
+});
