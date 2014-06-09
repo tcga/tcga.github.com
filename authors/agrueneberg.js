3dbd91a8ebe5d0850a51015355a0cd97871bf05a
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2014-06-09 16:51:55 -0500

    Update module locations.

--- authors/agrueneberg.js
+++ authors/agrueneberg.js
@@ -2,21 +2,21 @@ manifest([
     {
         "name": "RPPA",
         "description": "Real time analysis of reverse phase protein data.",
-        "url": "https://raw.github.com/agrueneberg/TCGA.rppa/master/rppa.js"
+        "url": "https://agrueneberg.github.io/TCGA.rppa/rppa.js"
     },
     {
         "name": "Import",
         "description": "Import your files and compare them to the reference data in TCGA.",
-        "url": "https://raw.github.com/agrueneberg/TCGA.import/master/import.js"
+        "url": "https://agrueneberg.github.io/TCGA.import/import.js"
     },
     {
         "name": "Dropbox",
         "description": "Access your Dropbox and load files into the TCGA Toolbox.",
-        "url": "https://raw.github.com/agrueneberg/TCGA.dropbox/master/dropbox.js"
+        "url": "https://agrueneberg.github.io/TCGA.dropbox/dropbox.js"
     },
     {
         "name": "Databases",
         "description": "Activate third-party databases for use with the TCGA Toolbox.",
-        "url": "https://raw.github.com/agrueneberg/TCGA.databases/master/databases.js"
+        "url": "https://agrueneberg.github.io/TCGA.databases/databases.js"
     }
 ]);
